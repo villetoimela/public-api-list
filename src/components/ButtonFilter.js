0030fa1a9681442a93307fc0a73c2ba4d6052d67
villetoimela <ville.toimela@hotmail.com>
2022-08-17 10:07:39 +0300

    added loading skeleton to ButtonFilter component for coolness

--- src/components/ButtonFilter.js
+++ src/components/ButtonFilter.js
@@ -9,6 +9,17 @@ const ButtonFilter = () => {
         fetchData();
     }, []);
 
+    if (loading) {
+        return (
+            <div className=" animate-pulse my-5 inline-block text-center">
+                {[...Array(35).keys()].map(num => (
+                    <div key={num} className="h-7 w-20 bg-gray-300 m-1
+                    rounded-md inline-block"></div>
+                ))}
+            </div>
+        )
+    }
+
     return (
         <div className="text-center my-10">
             {categories && categories.map(button => (
--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,7 @@ const header = () => {
           placeholder="Search APIs"
         />
         <button
-          className="bg-cyan-500 w-20 h-12 text-white rounded-r-md flex items-center justify-center">
+          className="bg-sky-400 w-20 h-12 text-white rounded-r-md flex items-center justify-center">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
             <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
           </svg>
