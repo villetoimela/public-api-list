45da4eb91606a6d3aa32cca5a7306b7744135386
villetoimela <ville.toimela@hotmail.com>
2022-08-31 10:13:53 +0300

    used new components in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import ApiList from "./components/ApiList";
 import ButtonFilter from "./components/ButtonFilter";
 import Header from "./components/Header";
+import useAxios from "./hooks/useAxios";
+import { useEffect } from "react";
 
 function App() {
+  const { fetchData, response, loading } = useAxios('entries');
+
+  useEffect(() => {
+    fetchData({ params: { category: 'Animals' } });
+  }, []);
+  
+
+
   return (
-    <div>
-      <Header />
-      <div className="container mx-auto max-w-3xl">
-        <ButtonFilter />
+    <div className="">
+      <Header fetchData={fetchData} />
+      <div className="container mx-auto max-w-6xl">
+        <ButtonFilter fetchData={fetchData} />
+        <ApiList response={response} loading={loading} />
       </div>
     </div>
   );
